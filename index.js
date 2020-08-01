710bc5e8408e7a2c11610f6eca15992b6d371949
Austin Greer <agreer.ag12@gmail.com>
2020-07-31 20:57:45 -0400

    completed task six

--- index.js
+++ index.js
@@ -110,9 +110,18 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    /* code here */
+function getReviewByIndex(array, index) {
+  
+  console.log(`${array[index].name} gave the restaurant a ${array[index].rating}, and their feedback was: ${array[index].feedback}`)
+
   }
+
+  getReviewByIndex(reviews, 3);
+
+
+
+
+  
   
 
 
